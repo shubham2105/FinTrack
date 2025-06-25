3c762a54cf32ca5d1b79268a31d3646ad4ca9c9c
shubham2105 <shubhamdhole215@gmail.com>
2025-06-25 23:41:25 +0530

    Stack Navigation Setup done

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import AppNavigator from './navigtion/AppNavigator'
 
 const FinTrack = () => {
   return (
-    <View style={styles.container}>
-      <Text>FinTrack</Text>
-    </View>
+    <AppNavigator />
   )
 }
 
 export default FinTrack
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
-  }
-})
